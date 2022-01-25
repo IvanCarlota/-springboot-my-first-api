ce8f93de127f7c49981474717a24826046fd9e72
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2022-01-24 21:34:11 -0300

    Implementação de tratamento de exceções

--- myapi/src/main/java/com/ivan/myapi/services/UsuarioService.java
+++ myapi/src/main/java/com/ivan/myapi/services/UsuarioService.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Component;
 
 import com.ivan.myapi.domain.Usuario;
 import com.ivan.myapi.repositories.UsuarioRepository;
+import com.ivan.myapi.services.exceptions.ObjectNotFoundException;
 
 @Component
 public class UsuarioService {
 
-	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	
+
 	public Usuario findById(Integer id) {
 		Optional<Usuario> obj = usuarioRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrato! Id: " + id + ", Tipo: " + Usuario.class.getName()));
 	}
-	
+
 }
